bba1fb0c5acb13649697816d335e19ff96f500dd
Efi Merdler-Kravitz <efi.merdler@gmail.com>
2023-09-04 14:46:28 +0000

    Add rust api

--- s3-admin-app/list_buckets_python/app.py
+++ s3-admin-app/list_buckets_python/app.py
@@ -7,14 +7,18 @@ logger = Logger()
 app = APIGatewayRestResolver()
 s3_client = boto3.client("s3")
 
+@logger.inject_lambda_context(log_event=True)
+def lambda_handler(event, context):
+    return app.resolve(event, context)
 
-@app.get("/buckets")
-def get_buckets() -> List[dict]:
-    s3_buckets = s3_client.list_buckets()["Buckets"]
 
+@app.get("/buckets-python")
+def get_buckets_python() -> List[dict]:
+    s3_buckets = s3_client.list_buckets()
+    print(s3_buckets)
     bucket_info_list = []
 
-    for bucket in s3_buckets:
+    for bucket in s3_buckets["Buckets"]:
         bucket_name = bucket["Name"]
         bucket_region = (
             s3_client.get_bucket_location(Bucket=bucket_name)["LocationConstraint"]
@@ -24,8 +28,3 @@ def get_buckets() -> List[dict]:
         bucket_info_list.append({"name": bucket_name, "region": bucket_region})
 
     return bucket_info_list
-
-
-@logger.inject_lambda_context(log_event=True)
-def lambda_handler(event, context):
-    return app.resolve(event, context)
