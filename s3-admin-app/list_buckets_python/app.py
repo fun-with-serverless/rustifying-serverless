a85fe2ab12ad89f7dbdf61d6a08b63e9d89101c8
Efi Merdler-Kravitz <efi.merdler@gmail.com>
2023-09-17 17:48:42 +0000

    Add async support

--- s3-admin-app/list_buckets_python/app.py
+++ s3-admin-app/list_buckets_python/app.py
@@ -1,11 +1,16 @@
-from typing import List
+import asyncio
+from typing import List, Tuple
 from aws_lambda_powertools import Logger
 from aws_lambda_powertools.event_handler import APIGatewayRestResolver
 import boto3
+from aiobotocore.session import get_session
+from aiobotocore.config import AioConfig
+import uvloop
 
 logger = Logger()
 app = APIGatewayRestResolver()
 s3_client = boto3.client("s3")
+
 
 @logger.inject_lambda_context(log_event=True)
 def lambda_handler(event, context):
@@ -15,16 +20,33 @@ def lambda_handler(event, context):
 @app.get("/buckets-python")
 def get_buckets_python() -> List[dict]:
     s3_buckets = s3_client.list_buckets()
-    print(s3_buckets)
     bucket_info_list = []
 
-    for bucket in s3_buckets["Buckets"]:
-        bucket_name = bucket["Name"]
-        bucket_region = (
-            s3_client.get_bucket_location(Bucket=bucket_name)["LocationConstraint"]
-            or "us-east-1"
-        )
-
-        bucket_info_list.append({"name": bucket_name, "region": bucket_region})
+    with asyncio.Runner(loop_factory=uvloop.new_event_loop) as runner:
+        bucket_info_list = runner.run(get_buckets_region(s3_buckets["Buckets"]))
 
     return bucket_info_list
+
+
+async def get_buckets_region(buckets: List[str]) -> List[Tuple[str, str]]:
+    session = get_session()
+    bucket_info_list = []
+    
+    async with session.create_client("s3", config=AioConfig(retries={"max_attempts": 0}) ) as s3_client:
+        tasks = [
+            get_bucket_info(s3_client, bucket["Name"]) for bucket in buckets
+        ]
+
+        # Run the batch of tasks and collect results
+        results = await asyncio.gather(*tasks)
+
+    # Add results to bucket_info_list
+    bucket_info_list.extend(results)
+
+    return bucket_info_list
+
+
+async def get_bucket_info(s3_client, bucket_name: str):
+        bucket_location = await s3_client.get_bucket_location(Bucket=bucket_name)
+        region = bucket_location["LocationConstraint"] or "us-east-1"
+        return {"name": bucket_name, "region": region}
--- s3-ops-rust-lib/src/lib.rs
+++ s3-ops-rust-lib/src/lib.rs
@@ -1,4 +1,5 @@
-use aws_sdk_s3::{Client, Error};
+use aws_sdk_s3::{types::BucketLocationConstraint, Client, Error};
+use futures::{stream::FuturesUnordered, StreamExt};
 use pyo3::{exceptions::PyValueError, prelude::*, types::PyDict};
 use tokio::runtime::Runtime;
 
@@ -34,20 +35,33 @@ impl S3OpsRust {
 async fn list_buckets_internal(client: &Client) -> Result<Vec<(String, String)>, Error> {
     let resp = client.list_buckets().send().await?;
     let buckets = resp.buckets().unwrap_or_default();
-
-    let mut bukcet_locations = Vec::new();
+    let tasks = FuturesUnordered::new();
     for bucket in buckets {
         let bucket_name = bucket.name().unwrap_or_default();
-        let resp = client
-            .get_bucket_location()
-            .bucket(bucket_name)
-            .send()
-            .await?;
-        let location = String::from(resp.location_constraint().unwrap().clone().as_str());
-        bukcet_locations.push((String::from(bucket_name), location));
-    }
+        let task = client.get_bucket_location().bucket(bucket_name).send();
+        let wrapped_task = async move {
+            let result = task.await;
+            match result {
+                Ok(response) => {
+                    let location = response
+                        .location_constraint()
+                        .unwrap_or(&BucketLocationConstraint::UsWest1)
+                        .as_str();
+                    Ok((String::from(bucket_name.clone()), String::from(location)))
+                }
+                Err(e) => Err(e),
+            }
+        };
 
-    Ok(bukcet_locations)
+        tasks.push(wrapped_task);
+    }
+    let results = tasks.collect::<Vec<_>>().await;
+    let bucket_locations: Vec<(String, String)> = results
+        .into_iter()
+        .filter_map(Result::ok)
+        .collect();
+
+    Ok(bucket_locations)
 }
 
 /// A Python module implemented in Rust.
