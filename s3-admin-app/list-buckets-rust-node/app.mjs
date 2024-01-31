8f521b36a0243fe38aa80c109fb3535a6cae55e4
Efi Merdler-Kravitz <efi.merdler@gmail.com>
2024-01-31 20:45:08 +0000

    Move init outside of handler

--- s3-admin-app/list-buckets-rust-node/app.mjs
+++ s3-admin-app/list-buckets-rust-node/app.mjs
@@ -1,10 +1,10 @@
 import {S3OpsRust} from "./index.js"
 
+const s3ops = new S3OpsRust();
 
 export const lambdaHandler = async (event, context) => {
 
     try {
-        const s3ops = new S3OpsRust();
         const results = s3ops.listBuckets();
         return {
             statusCode: 200,
